dc6dd26c1f4eb9332295b826068043d39be11615
toofanjhala <jhalatoofan@gmail.com>
2023-02-21 17:47:31 +0530

    handling error

--- src/App.js
+++ src/App.js
@@ -7,27 +7,45 @@ function App() {
   const [movies, setmovies] = useState([])
   const [isloading, setisloading] = useState(false)
   const [error, seterror] = useState(null)
+  const [intervalId, setIntervalId] = useState(0);
+
+
 
   async function fetchingdatahandler() {
     seterror(null)
     setisloading(true)
-    try {
 
-      const response = await fetch("https://swapi.py4e.com/api/film/")
+  const interval = setInterval(async () => {
 
-      if (response.status !== 200) {
-        throw new Error("something went wrong retrying...")
+      try {
+        const response = await fetch("https://swapi.py4e.com/api/film/")
+
+        if (response.status !== 200) {
+          throw new Error("something went wrong retrying...")
+        }
+
+
+        const convertedresponse = await response.json()
+        const moviesarray = await convertedresponse.results
+        setisloading(false)
+        setmovies(moviesarray)
+      } catch (Error) {
+        seterror(Error.message)
+
       }
-      const convertedresponse = await response.json()
-      const moviesarray = await convertedresponse.results
-      setisloading(false)
-      setmovies(moviesarray)
-    } catch (Error) {
-      seterror(Error.message)
+    }, 5000);
+    setIntervalId(interval)
 
-    }
 
   }
+
+  function stophandler(){
+clearInterval(intervalId)
+  setIntervalId(111)
+  }
+
+
+console.log("out",intervalId)
 
   let content = <p> NO movie Available</p>
 
@@ -37,9 +55,14 @@ function App() {
   }
 
   if (error) {
-    content = error
+    content = <div><button onClick={stophandler}>Cancel</button> <p>{error}</p></div>
   }
 
+
+  if(intervalId===111)
+  {
+    content= <p> sorry , no item found</p>
+  }
   if (movies.length > 0) {
     content = <MoviesList movies={movies} />
   }
