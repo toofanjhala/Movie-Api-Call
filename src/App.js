938450c086fbfaca5aa097fabf27a87e7d2397ba
toofanjhala <jhalatoofan@gmail.com>
2023-02-21 13:24:28 +0530

    error handling of page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,50 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const [movies,setmovies]=useState([])
+  const [movies, setmovies] = useState([])
+  const [isloading, setisloading] = useState(false)
+  const [error, seterror] = useState(null)
 
-  async function fetchingdatahandler(){
-    
-     const response= await  fetch("https://swapi.py4e.com/api/films/")
-     const convertedresponse = await response.json()
-     const moviesarray= await convertedresponse.results
-     
+  async function fetchingdatahandler() {
+    seterror(null)
+    setisloading(true)
+    try {
+
+      const response = await fetch("https://swapi.py4e.com/api/film/")
+
+      if (response.status !== 200) {
+        throw new Error("something went wrong retrying...")
+      }
+      const convertedresponse = await response.json()
+      const moviesarray = await convertedresponse.results
+      setisloading(false)
       setmovies(moviesarray)
-    
-     
-}
+    } catch (Error) {
+      seterror(Error.message)
+
+    }
+
+  }
+
+  let content = <p> NO movie Available</p>
+
+  if (isloading === true) {
+    content = <p> loading...</p>
+
+  }
+
+  if (error) {
+    content = error
+  }
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />
+  }
+
+
 
   return (
     <React.Fragment>
@@ -23,7 +52,7 @@ function App() {
         <button onClick={fetchingdatahandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {content}
       </section>
     </React.Fragment>
   );
